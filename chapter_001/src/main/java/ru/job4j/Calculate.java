ed8f73544712a83d6762d69da32c7e63163cce61
Ilya Bukreyev <ilyabuk@gmail.com>
2017-03-18 19:44:02 +0300

    add package-info

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,26 @@
+/**
+* Calculator
+*/
+
 package ru.job4j;
 
+/**
+* Решение задачи часть 1.1.1.3
+* Calculate.
+* @author Bukreyev Ilya
+* @since 18/03/2017
+* @version 1.0
+*/
+
 public class Calculate {
-	public static void main (String [] args){
-	System.out.print ("HelloWorld!!");
+	int result;
+	/**
+	* addition, takes two arguments and assigns
+	* result to variable "result"
+	* @param a - first argument
+	* @param b - second argument
+	*/
+	void addition (int a, int b) {
+		result = a + b;
 	}
 }
