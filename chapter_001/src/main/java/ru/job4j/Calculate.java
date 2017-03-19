eafb143d7e18f5d81415565bc3e89550f50849e5
Ilya Bukreyev <ilyabuk@gmail.com>
2017-03-20 00:07:30 +0300

    connect CheckStyle

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,23 +1,23 @@
+
 /**
 * Calculator
 */
-
 package ru.job4j;
 
-/**
+/**.
 * Решение задачи часть 1.1.1.3
-* Calculate.
+* Calculate
 * @author Bukreyev Ilya
 * @since 18/03/2017
 * @version 1.0
 */
-
 public class Calculate {
-	/**
-	* main
-	* @param - String args []
-	*/
-	public static void main (String [] args) {
-		System.out.print ("Hello World!");
+
+/**.
+* main
+* @param args is args
+*/
+	public static void main(String[] args) {
+		System.out.print("Hello World!");
 	}
 }
