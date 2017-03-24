8735114746e0eeb775c65c7e8eb098ea2c3f36a6
Ilya Bukreyev <ilyabuk@gmail.com>
2017-03-24 16:37:13 +0300

    Improve Max

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -2,16 +2,13 @@
 Maximum of two numbers.
 */
 package ru.job4j.max;
-
 /**
 Class Max provides method, that returns maximum of two numbers.
 @since 22/03/2017
 @version 1.0
 @author Bukreyev Ilya
 */
-
 class Max {
-
   /**
   method max return one number which is greather than another.
   @param first number
@@ -20,5 +17,15 @@ class Max {
   */
   public int max(int first, int second) {
     return first > second ? first : second;
+  }
+  /**
+  @param first number
+  @param second number
+  @param third number
+  @return max of numbers
+  */
+  public int max(int first, int second, int third) {
+    int temp = max(first, second);
+    return max(temp, third);
   }
 }
--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -32,4 +32,34 @@ first, should return second of them.
     int expected = 6;
     assertThat(result, is(expected));
   }
+  /**
+  extended test for another method - 3 numbers #1.
+  */
+  @Test
+  public void compareThreeNumbersReturnGreathest1() {
+    Max obj = new Max();
+    int result = obj.max(5, 9, 3);
+    int expected = 9;
+    assertThat(result, is(expected));
+  }
+  /**
+  extended test for another method - 3 numbers #2.
+  */
+  @Test
+  public void compareThreeNumbersReturnGreathest2() {
+    Max obj = new Max();
+    int result = obj.max(9, 5, 3);
+    int expected = 9;
+    assertThat(result, is(expected));
+  }
+  /**
+  extended test for another method - 3 numbers #3.
+  */
+  @Test
+  public void compareThreeNumbersReturnGreathest3() {
+    Max obj = new Max();
+    int result = obj.max(5, 3, 9);
+    int expected = 9;
+    assertThat(result, is(expected));
+  }
 }
