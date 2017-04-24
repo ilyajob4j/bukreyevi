ea2724949573c74a029b1822bf9e1b20d53e82b0
Ilya Bukreyev <ilyabuk@gmail.com>
2017-04-25 00:53:43 +0300

    ConsoleInput2

--- chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -1,84 +1,27 @@
 /**
-   package-info.
+ * package-info.
  */
 package ru.job4j.tracker;
 
 import java.util.Scanner;
+
 /**
  * class info.
  */
-public class ConsoleInput {
+class ConsoleInput {
 /**
-   new tracker for session.
+   @param scanner is scanner.
  */
-private Tracker tracker = new Tracker();
-/**
-   @param b - keeps dialog live.
- */
-private boolean b = true;
-/**
-   actually main user class.
- */
-public void start() {
-        while (b) {
-                System.out.println("Добро пожаловать в систему заявок. Доступные варианты:\n1. Добавить заявку\n2. Обновить заявку (нужно указать обновляемый элемент)\n3. Удаление заявки по номеру\n4. Получить список всех заявок\n5. Найти заявку по названию\n6. Найти заявку по номеру\n0. Закончить работу с приложением\n Выберите вариант");
-                int mainMenuCase = userIntInput();
-                switch (mainMenuCase) {
-                case 1: tracker.add(setItemFromConsole());
-                        System.out.println("Добавили, возвращаемся в главное меню.");
-                        break;
-                case 2: tracker.update(setItemFromConsole());
-                        System.out.println("Обновили, возвращаемся в главное меню.");
-                        break;
-                case 3: System.out.println("Укажите номер заявки для удаления");
-                        tracker.delete(tracker.getItemId(userIntInput()));
-                        System.out.println("Удаление наверное произошло, возвращаемся в главное меню");
-                        break;
-                case 4: for (Item i : tracker.findAll()) {
-                                System.out.print(i + ",");
-                }
-                        System.out.println("\nНапечатали, возвращаемся в главное меню.");
-                        break;
-                case 5: for (Item i : tracker.findByName(userStringInput())) {
-                                System.out.println(i + ",");
-                }
-                        System.out.println("\nНапечатали, возвращаемся в главное меню.");
-                        break;
-                case 6: System.out.println(tracker.findById(userStringInput()));
-                        System.out.println("Напечатали, возвращаемся в главное меню.");
-                        break;
-                case 0: System.out.println("До новых встреч!");
-                        b = false;
-                        break;
-                default: System.out.println("Введен недопустимый вариант");
-                }
-        }
-}
-
+private Scanner scanner = new Scanner(System.in);
 
 /**
-   user string input.
-   @return string input.
+ * actually main user class.
+ *
+ * @param text - main menu.
+   @return text.
  */
-public String userStringInput() {
-        Scanner ss = new Scanner(System.in);
-        return ss.next();
-}
-/**
-   user num input.
-   @return int input.
- */
-public int userIntInput() {
-        Scanner s = new Scanner(System.in);
-        return s.nextInt();
-}
-/**
-   @return specified by user item.
- */
-public Item setItemFromConsole() {
-        System.out.println("Введите имя заявки");
-        Item item = new Item();
-        item.setName(userStringInput());
-        return item;
+String start(String text) {
+        System.out.println(text);
+        return scanner.next();
 }
 }
--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,17 +1,95 @@
 /**
-   package-info.
+ * package-info.
  */
 package ru.job4j.tracker;
+
+import java.util.Scanner;
+
 /**
-   class info.
+ * class info.
  */
 public class StartUI {
 /**
-   main.
-   @param args - arguments.
+   @param b keeps dialog alive.
+ */
+private static boolean b = true;
+/**
+ * main.
+ *
+ * @param args - arguments.
  */
 public static void main(String[] args) {
-        ConsoleInput obj = new ConsoleInput();
-        obj.start();
+        ConsoleInput consoleInput = new ConsoleInput();
+        Tracker tracker = new Tracker();
+        while (b) {
+                String name = consoleInput.start("Добро пожаловать в систему заявок. Доступные варианты:\n1. Добавить заявку\n2. Обновить заявку (нужно указать обновляемый элемент)\n3. Удаление заявки по номеру\n4. Получить список всех заявок\n5. Найти заявку по названию\n6. Найти заявку по номеру\n0. Закончить работу с приложением\n Выберите вариант");
+                switch (name) {
+                case "1":
+                        tracker.add(setItemFromConsole());
+                        System.out.println("Добавили, возвращаемся в главное меню.");
+                        break;
+                case "2":
+                        tracker.update(setItemFromConsole());
+                        System.out.println("Обновили, возвращаемся в главное меню.");
+                        break;
+                case "3":
+                        System.out.println("Укажите номер заявки для удаления");
+                        tracker.delete(tracker.getItemId(userIntInput()));
+                        System.out.println("Удаление наверное произошло, возвращаемся в главное меню");
+                        break;
+                case "4":
+                        for (Item i : tracker.findAll()) {
+                                System.out.print(i + ",");
+                        }
+                        System.out.println("\nНапечатали, возвращаемся в главное меню.");
+                        break;
+                case "5":
+                        for (Item i : tracker.findByName(userStringInput())) {
+                                System.out.println(i + ",");
+                        }
+                        System.out.println("\nНапечатали, возвращаемся в главное меню.");
+                        break;
+                case "6":
+                        System.out.println(tracker.findById(userStringInput()));
+                        System.out.println("Напечатали, возвращаемся в главное меню.");
+                        break;
+                case "0":
+                        System.out.println("До новых встреч!");
+                        b = false;
+                        break;
+                default:
+                        System.out.println("Введен недопустимый вариант");
+                }
+        }
+}
+
+/**
+ * user string input.
+ *
+ * @return string input.
+ */
+private static String userStringInput() {
+        Scanner ss = new Scanner(System.in);
+        return ss.next();
+}
+
+/**
+ * user num input.
+ *
+ * @return int input.
+ */
+private static int userIntInput() {
+        Scanner s = new Scanner(System.in);
+        return s.nextInt();
+}
+
+/**
+ * @return specified by user item.
+ */
+private static Item setItemFromConsole() {
+        System.out.println("Введите имя заявки");
+        Item item = new Item();
+        item.setName(userStringInput());
+        return item;
 }
 }
